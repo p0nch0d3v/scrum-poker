ceb3a56e843e9a03eb7d100b4f13a20f59fd339b
p0nch0 d3v <p0nch0.d3v@gmail.com>
2024-02-20 19:45:26 -0800

    api and proxy implementation

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -6,6 +6,12 @@ export default defineConfig({
   plugins: [preact()],
   server: {
     host: "0.0.0.0",
-    port: 5173
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3000',
+        changeOrigin: true
+      }
+    }
   }
 })
