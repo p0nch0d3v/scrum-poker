54c9733f8a9ed33ea1a566322d629428ff767f72
p0nch0 d3v <p0nch0.d3v@gmail.com>
2024-02-23 12:44:19 -0800

    postgres docker update

--- postgress_docker.sh
+++ postgress_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
 echo $1
-container_name=local-postgres
-volume_name=local-postgres-volume
-docker_image='postgres:16'
+container_name=scrum-poker-local
+volume_name=scrum-poker-local-volume
+docker_image='postgres:16.1'
 env_file='.env.docker'
 
 set_environment () {
@@ -27,26 +27,37 @@ set_volume () {
   fi
 }
 
-docker pull $docker_image
-set_environment
-set_volume
+stop () {
+  docker container stop $container_name
+  docker container rm $container_name
+}
+
+start () {
+  docker pull $docker_image
+  set_environment
+  set_volume
+
+  docker run \
+      --name $container_name \
+      --publish $POSTGRES_PORT:5432 \
+      --env POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
+      --env POSTGRES_USER=$POSTGRES_USER \
+      --env POSTGRES_DB=$POSTGRES_DB \
+      --volume "$volume_name:/var/lib/postgresql/data" \
+      --detach \
+      $docker_image
+
+      docker container list --filter name=$container_name --format '{{.Names}}'
+      docker container list --filter name=$container_name --format '{{.Status}}'
+
+      docker container logs --follow $container_name
+}
 
 if [[ $1 == "stop" ]]; then
-    docker container stop $container_name
-    docker container rm $container_name
+  stop  
 fi
 
 if [[ $1 == "start" ]]; then
-    docker run \
-        --name $container_name \
-        --publish 5432:5432 \
-        --env POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
-        --env POSTGRES_USER=$POSTGRES_USER \
-        --env POSTGRES_DB=$POSTGRES_DB \
-        --volume "$volume_name:/var/lib/postgresql/data" \
-        --detach \
-        $docker_image
-
-        docker container list --filter name=$container_name --format '{{.Names}}'
-        docker container list --filter name=$container_name --format '{{.Status}}'
+  stop
+  start
 fi
