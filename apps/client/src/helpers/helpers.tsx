f0cea12c4f7157b4e66a172d5d6c3c34b90bfcc7
p0nch0 d3v <p0nch0.d3v@gmail.com>
2024-03-05 18:32:52 -0800

    client production loggin disabled

--- apps/client/src/helpers/helpers.tsx
+++ apps/client/src/helpers/helpers.tsx
@@ -1,3 +1,5 @@
+import Config from "../config/config";
+
 const validateUUID = function (uuid: string): boolean {
     const regexV1 = /^[0-9A-F]{8}-[0-9A-F]{4}-[1][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
     const regexV2 = /^[0-9A-F]{8}-[0-9A-F]{4}-[2][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
@@ -8,11 +10,17 @@ const validateUUID = function (uuid: string): boolean {
     return regexV1.test(uuid) || regexV2.test(uuid) || regexV3.test(uuid) || regexV4.test(uuid) || regexV5.test(uuid)
 };
 
-const isUndefinedNullOrEmpty = function(value: string | undefined): boolean {
+const isUndefinedNullOrEmpty = function (value: string | undefined): boolean {
     return typeof value === 'undefined'
         || value === undefined
         || value === null
         || value.length === 0;
+}
+
+if (Config.IS_PRODUCTION) {
+    window.console.log = () => { };
+    window.console.debug = () => { };
+    window.console.info = () => { };
 }
 
 export {
