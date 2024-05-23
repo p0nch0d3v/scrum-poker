3e853345c94973a2b0e48f6e44096a5560f404f7
p0nch0 d3v <p0nch0.d3v@gmail.com>
2024-05-23 14:41:25 -0700

    correct hide un-hide beHAVIOR

--- apps/models/DTO/roomInfo.dto.ts
+++ apps/models/DTO/roomInfo.dto.ts
@@ -1,4 +1,10 @@
 
-export type RoomInfoDTO = {
-    roomId: string
+export class RoomInfoDTO {
+    constructor(id: string, hide: boolean) {
+        this.roomId = id;        
+        this.hide = hide;
+    }
+
+    roomId: string;
+    hide: boolean;
 };
