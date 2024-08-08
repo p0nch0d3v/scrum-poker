a64983cc41422772ec482bb14d0c190780ecb470
p0nch0 d3v <p0nch0.d3v@gmail.com>
2024-08-08 03:22:31 +0000

    ui fixes

--- apps/client/src/helpers/helpers.tsx
+++ apps/client/src/helpers/helpers.tsx
@@ -46,16 +46,20 @@ function reverseString(input: string): string {
     return input.split("").reverse().join("");
 }
 
-const getShortName = (input: string): string => {
+const getShortName = (input: string, count: number): string => {
     if (isUndefinedNullOrEmpty(input)) {
         return "";
     }
     const subItems = input.split(" ");
     if (subItems.length > 1) {
-        return subItems[0].substring(0, 1) + subItems[1].substring(0, 1);
+        let returnValue: string = ""
+        for (let i = 0; i < Math.min(count, subItems.length); i++) {
+            returnValue += subItems[i].substring(0, 1);
+        }
+        return returnValue;
     }
     else if (subItems[0].length > 1) {
-        return subItems[0].substring(0, 2);
+        return subItems[0].substring(0, Math.min(count, subItems[0].length));
     }
     return subItems[0];
 }
