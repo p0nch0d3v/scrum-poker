2879770902aa7d52b12021130038bfea071e8a88
p0nch0 d3v <p0nch0.d3v@gmail.com>
2024-10-25 02:00:27 +0000

    google nickname regex update

--- apps/common/index.ts
+++ apps/common/index.ts
@@ -1,10 +1,10 @@
 
 const getGoogleNickname = function (input: string): string {
-    const regex: RegExp = /(\".+\")|(\(.+\))/gmi;
+    const regex: RegExp = /(\".+\")|(\“.+\”)|(\(.+\))/gmi;
     const result = input.match(regex);
     if (result && result.length > 0) {
         let match: string = result[0];
-        const regexToClean: RegExp = /(\"|\(|\))/gmi;
+        const regexToClean: RegExp = /(\"|\“|\”|\(|\))/gmi;
 
         while (match.match(regexToClean)) {
             match = match.replace(regexToClean, '');
